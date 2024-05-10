7950dea40ea20848ee99f96799748233a75f7429
ApurbM <apurbmishra47@gmail.com>
2024-05-10 12:05:26 +0530

    my secound git folder

---
+++ Apurb Mishra/add.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+int a;
+int b;
+cout<<"Enter your first number"<<endl;
+cin>>a;
+cout<<"Enter your secound number"<<endl;
+cin>>b;
+cout<<"sum of "<<a<<" and "<<b<<" is "<<a+b<<endl;
+return 0;
+}
